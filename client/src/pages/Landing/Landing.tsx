61fc4fb56afbc3a7c806826901159888d13ed315
Aishwarya Khandare <aishwaryakhandare005@gmail.com>
2026-07-05 13:54:50 +0530

    Landing.tsx rewritten

--- client/src/pages/Landing/Landing.tsx
+++ client/src/pages/Landing/Landing.tsx
@@ -1,23 +1,11 @@
 import Navbar from "../../components/landing/Navbar/Navbar";
 import Hero from "../../components/landing/Hero/Hero";
-import TrustedCompanies from "../../components/landing/TrustedCompanies/TrustedCompanies";
-import DashboardShowcase from "../../components/landing/DashboardShowcase/DashboardShowcase";
-import HowItWorks from "../../components/landing/HowItWorks/HowItWorks";
-import Features from "../../components/landing/Features/Features";
-import CTA from "../../components/landing/CTA/CTA";
-import Footer from "../../components/landing/Footer/Footer";
 
 function Landing() {
   return (
     <>
       <Navbar />
       <Hero />
-      <TrustedCompanies />
-      <DashboardShowcase />
-      <HowItWorks />
-      <Features />
-      <CTA />
-      <Footer />
     </>
   );
 }
